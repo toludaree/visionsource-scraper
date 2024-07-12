109e39526222f6049943fb232b8e6c5dc374c5f1
Isaac-Tolu <isaactoluwani30@gmail.com>
2024-07-13 00:43:16 +0100

    create output processor for `address` field

--- visionsource/visionsource/items.py
+++ visionsource/visionsource/items.py
@@ -13,11 +13,14 @@ def doctors_in(d:str):
 def doctors_out(d:str):
     return d.strip()
 
+def address_out(d:list[str]):
+    return "; ".join(d)
+
 
 class VisionsourceItem(Item):
     business_name = Field(output_processor=Join())
     doctors = Field(input_processor=MapCompose(doctors_in),
                     output_processor=MapCompose(doctors_out))
-    address = Field(output_processor=Join())
+    address = Field(output_processor=address_out)
     phone_number = Field(output_processor=Join())
     website = Field(output_processor=Join())
