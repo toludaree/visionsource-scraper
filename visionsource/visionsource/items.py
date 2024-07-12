9748b7302d67fe1f08c340cb8fe01dacbf896321
Isaac-Tolu <isaactoluwani30@gmail.com>
2024-07-13 00:34:58 +0100

    create custom input and output processor for `doctors` field

--- visionsource/visionsource/items.py
+++ visionsource/visionsource/items.py
@@ -4,12 +4,20 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 from scrapy import Item, Field
-from itemloaders.processors import Join
+from itemloaders.processors import Join, MapCompose
+
+
+def doctors_in(d:str):
+    return d.split(",")
+
+def doctors_out(d:str):
+    return d.strip()
 
 
 class VisionsourceItem(Item):
     business_name = Field(output_processor=Join())
-    doctors = Field(output_processor=Join())
+    doctors = Field(input_processor=MapCompose(doctors_in),
+                    output_processor=MapCompose(doctors_out))
     address = Field(output_processor=Join())
     phone_number = Field(output_processor=Join())
     website = Field(output_processor=Join())
